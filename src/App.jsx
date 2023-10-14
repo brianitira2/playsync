4d138da5ef27531fa2fc4969c8448cff9b82aa2f
Brian-Itira Itira <brianitira@gmail.com>
2023-10-14 12:55:22 +0000

    file and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
 
-const App = () => {
-  return (
-    <div>app</div>
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import SearchFeed from './components/SearchFeed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+
+const App = () =>
+ (
+   <BrowserRouter>
+   <Box sx={{backgroundColor: '#000'}}>
+    <Navbar/>
+
+    <Routes>
+      <Route path= "/" exact element = {<Feed/>}/>
+      <Route path= "/video/:id" exact element = {<VideoDetail/>}/>
+      <Route path= "/channel/:id" exact element = {<ChannelDetail/>}/>
+      <Route path= "/search/searchTerm" exact element = {<SearchFeed/>}/>
+    </Routes>
+
+   </Box>
+   </BrowserRouter>
   )
-}
+
 
 export default App
 
