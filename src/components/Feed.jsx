005695a2bfdd9b89140d2d8a3b319987a9f5c63d
Brian-Itira Itira <brianitira@gmail.com>
2023-10-14 14:21:13 +0000

    regular commits 2

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+import Sidebar from "./Sidebar";
 
-export default Feed
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
+        <p>
+          © 2023 StreamPulse - Elevating Your Video Experience. All Rights
+          Reserved.
+          <br />
+          Created by Brian Itira
+        </p>
+      </Typography>
+    </Box>
+  </Stack>
+);
+
+export default Feed;
