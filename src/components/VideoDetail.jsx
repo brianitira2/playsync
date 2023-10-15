79091882f85ef5135828d1c048a9b9392e5ed354
Brian-Itira Itira <brianitira@gmail.com>
2023-10-15 22:27:10 +0000

    modifications on the media player

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,25 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { fetchFromApi } from '../utils/Api';
 import { useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
-import { Box, Typography, Paper } from '@mui/material';
+import { Box, Typography, Paper, Grid } from '@mui/material';
+import Videos from './Videos';
 
 const VideoDetail = () => {
   const { id } = useParams();
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
+    // Fetch video details
     fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
       .then((data) => setVideoDetail(data.items[0]));
+
+    // Fetch related videos
+    fetchFromApi(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items));
   }, [id]);
 
   return (
-    <Box minHeight="95vh" display="flex" flexDirection="column">
-      <Box mt={2} flex="1" display="flex" position='sticky'>
-        <div style={{ width: '100%', maxWidth: '900px' }}>
+    <Grid container>
+      <Grid item xs={12} sm={12} md={6} >
+        <Box width="100%" maxWidth="1000px">
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${id}`}
             width="100%"
-            height="100%"
+            height="700px"
             controls={true}
             config={{
               youtube: {
@@ -32,17 +39,24 @@ const VideoDetail = () => {
               },
             }}
           />
-        </div>
-      </Box>
-      <Box mt={-0.1} flex="1" maxWidth="900px" >
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6} margin='0 auto'>
         {videoDetail && (
-          <Paper elevation={3} sx={{ p: 2, color: 'green' }}>
+          <Paper elevation={3} sx={{ p: 2, backgroundColor: 'white', borderRadius: '8px' }}>
             <Typography variant="h5">{videoDetail.snippet.title}</Typography>
             <Typography variant="body2">{videoDetail.snippet.description}</Typography>
           </Paper>
         )}
+      </Grid>
+      <Box sx={{ flex: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Grid container spacing={-1}>
+          <Grid item xs={12} sm={8} md={9} margin='0 auto'>
+            <Videos videos={videos}/>
+          </Grid>
+        </Grid>
       </Box>
-    </Box>
+    </Grid>
   );
 };
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Grid, CircularProgress } from '@mui/material';
 
 import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
 const Videos = ({ videos }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (videos) {
+      setIsLoading(false);
+    }
+  }, [videos]);
+
+  if (isLoading) {
+   
+    return <CircularProgress />;
+  }
+
   return (
     <Grid container spacing={1} justifyContent="center" >
       {videos.map((item, index) => (
-        <Grid item xs={12} sm={6} lg={3} key={index} >
+        <Grid item xs={12} sm={6} lg={3} key={index}>
           {item.id && item.id.videoId && <VideoCard video={item} />}
           {item.id && item.id.channelId && <ChannelCard channelDetail={item} />}
         </Grid>
