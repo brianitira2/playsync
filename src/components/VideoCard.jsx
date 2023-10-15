a10b5da346b5daaa71781e89919b79a903635004
Brian-Itira Itira <brianitira@gmail.com>
2023-10-15 12:14:36 +0000

    responsive design implementation

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,33 +17,46 @@ const VideoCard = ({
     snippet,
   },
 }) => {
-  return(
-  <Card>
-    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-      <CardMedia image={snippet?.thumbnails?.high?.url }alt={snippet?.title} sx={{width: 400, height: 200}}  />
-    </Link>
+  return (
+    <Card sx={{ maxWidth: 400, margin: "auto", height: "100%", display: 'flex', flexDirection: 'column' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: "100%", paddingTop: "56.25%" }} 
+        />
+      </Link>
 
-    <CardContent sx={{backgroundColor: '#161e16', height: '110px'}}>
+      <CardContent
+        sx={{
+          backgroundColor: '#161e16',
+          padding: '16px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '100%',
+        }}
+      >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-
-            <Typography variant="subtitle1" fontWeight='bold' color='#2ecc71'>
-                {snippet?.title.slice(0, 60)}
-            </Typography>
-
+          <Typography variant="subtitle1" fontWeight="bold" color='#2ecc71' sx={{ fontSize: {xs: 16, md: 18} }}>
+            {snippet?.title.slice(0, 60)}
+          </Typography>
         </Link>
-
 
         <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
-
-            <Typography variant="subtitle2" fontWeight='bold' color='gray'>
-                {snippet?.channelTitle.slice(0, 60)}
-                <CheckCircle sx={{fontSize: 18, color: 'white', ml: '10px'}}/>
-            </Typography>
-
+          <Typography
+            variant="subtitle2"
+            fontWeight="bold"
+            color='gray'
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            {snippet?.channelTitle.slice(0, 60)}
+            <CheckCircle sx={{ fontSize: 18, color: 'white', ml: '10px' }} />
+          </Typography>
         </Link>
-    </CardContent>
-  </Card>
-  )
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
