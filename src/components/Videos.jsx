49806b4f0a5f940ab0f94f66cbead65dabdd8af0
Brian-Itira Itira <brianitira@gmail.com>
2023-10-16 00:32:18 +0000

    regular commits

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -17,12 +17,29 @@ const Videos = ({ videos }) => {
     return <CircularProgress />;
   }
 
+  const preventSway = (item) => {
+    const aspectRatio = '16:9'; // Set the desired aspect ratio (e.g., 16:9 for widescreen)
+
+    return (
+      <div
+        style={{
+          position: 'relative',
+          paddingBottom: `calc(100% * ${aspectRatio.split(':')[1]} / ${aspectRatio.split(':')[0]})`,
+        }}
+      >
+        <div style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}>
+          {item.id && item.id.videoId && <VideoCard video={item} />}
+          {item.id && item.id.channelId && <ChannelCard channelDetail={item} />}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Grid container spacing={1}>
       {videos.map((item, index) => (
         <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-          {item.id && item.id.videoId && <VideoCard video={item} />}
-          {item.id && item.id.channelId && <ChannelCard channelDetail={item} />}
+          {preventSway(item)}
         </Grid>
       ))}
     </Grid>
